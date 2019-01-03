047d7e1a8878d185c78eacd6660380e75b9ced1c
Yahgo <yahgostm@hotmail.com>
2019-01-03 11:23:29 -0300

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		# sucesso
+  		flash[:success] = "Welcome to the Twitterzinho"
+  		redirect_to @user
   	else
   		render 'new'
   	end
